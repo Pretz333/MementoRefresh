5f36cd81d88714730992731989a8811ed519696c
Pretz333 <wpretzel@hotmail.com>
2021-02-11 01:33:18 -0600

    Laid out prep work for crouching and dynamic use
    Also improved code readability of the UnregisterForEvent(s)

--- MementoRefresh/MementoRefresh.lua
+++ MementoRefresh/MementoRefresh.lua
@@ -39,11 +39,7 @@ function MementoRefresh.slashCommander(command)
     MementoRefresh.savedVariables.mementoId = MementoRefresh.mementos[command].mementoId
     MementoRefresh.savedVariables.abilityId = MementoRefresh.mementos[command].abilityId
     MementoRefresh.savedVariables.delay = MementoRefresh.mementos[command].delay
-    if MementoRefresh.mementos[command].memento = "no memento" then
-      EVENT_MANAGER:UnregisterForEvent(MementoRefresh.name, EVENT_COMBAT_EVENT)
-    else
-      MementoRefresh.shouldRefresh()
-    end
+    MementoRefresh.shouldRefresh()
   elseif command == 'debug on' then
     EVENT_MANAGER:RegisterForEvent(MementoRefresh.name .. 'Debug', EVENT_COMBAT_EVENT, MementoRefresh.debugOn)
     EVENT_MANAGER:AddFilterForEvent(MementoRefresh.name .. 'Debug', EVENT_COMBAT_EVENT, REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE, COMBAT_UNIT_TYPE_NONE)
@@ -59,19 +55,44 @@ end
 
 function MementoRefresh.shouldRefresh()
   if MementoRefresh.savedVariables.mementoId ~= nil then
+    EVENT_MANAGER:RegisterForEvent(MementoRefresh.name .. "Result", EVENT_COLLECTIBLE_USE_RESULT, MementoRefresh.UseResult)
     EVENT_MANAGER:RegisterForEvent(MementoRefresh.name, EVENT_COMBAT_EVENT, MementoRefresh.mementoRanOut)
     EVENT_MANAGER:AddFilterForEvent(MementoRefresh.name, EVENT_COMBAT_EVENT, REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE, COMBAT_UNIT_TYPE_NONE)
     EVENT_MANAGER:AddFilterForEvent(MementoRefresh.name, EVENT_COMBAT_EVENT, REGISTER_FILTER_COMBAT_RESULT, ACTION_RESULT_EFFECT_FADED)
     -- EVENT_MANAGER:AddFilterForEvent(MementoRefresh.name, EVENT_COMBAT_EVENT, REGISTER_FILTER_ABILITY_ID, MementoRefresh.savedVariables.abilityId)
     UseCollectible(MementoRefresh.savedVariables.mementoId)
+  else
+    EVENT_MANAGER:UnregisterForEvent(MementoRefresh.name, EVENT_COMBAT_EVENT)
+    EVENT_MANAGER:UnregisterForEvent(MementoRefresh.name .. "Result", EVENT_COLLECTIBLE_USE_RESULT)
   end
+end
+
+function MementoRefresh.UseResult(_, result, isAttemptingActivation)
+  -- if result != COLLECTIBLE_USAGE_BLOCK_REASON_NOT_BLOCKED && isAttemptingActivation then
+    -- zo_callLater(MementoRefresh.refreshNow(), 1000)
+  -- end
 end
 
 -- (eventCode, result, isError, abilityName, abilityGraphic, abilityActionSlotType, sourceName, sourceType, targetName, targetType, hitValue, powerType, damageType, log, sourceUnitId, targetUnitId, abilityId, overflow)
 function MementoRefresh.mementoRanOut(_, result, _, abilityName, _, _, _, sourceType, _, _, _, _, _, _, _, _, abilityId)
   if abilityId == MementoRefresh.savedVariables.abilityId then
-    zo_callLater(function() UseCollectible(MementoRefresh.savedVariables.mementoId) end, 1000 + MementoRefresh.savedVariables.delay) 
+    -- if is crouching
+      -- EVENT_MANAGER:RegisterForUpdate(MementoRefresh.name .. "Crouch", 1500, MementoRefresh.crouchCheck)
+    -- else
+    zo_callLater(MementoRefresh.refreshNow(), 1000 + MementoRefresh.savedVariables.delay)
+    -- end
   end
+end
+
+function MementoRefresh.crouchCheck()
+  -- if not crouching
+    -- EVENT_MANAGER:UnregisterForUpdate(MementoRefresh.name .. "Crouch")
+    -- MementoRefresh.refreshNow()
+  -- end
+end
+
+function MementoRefresh.refreshNow()
+  UseCollectible(MementoRefresh.savedVariables.mementoId)
 end
 
 function MementoRefresh.debugOn(_, result, _, abilityName, _, _, _, sourceType, _, _, _, _, _, _, _, _, abilityId)
