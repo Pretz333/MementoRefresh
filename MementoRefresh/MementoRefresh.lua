9f00df2df0e0f2e58e18ea393b74632104cb02b7
Pretz333 <wpretzel@hotmail.com>
2024-07-01 19:46:29 -0500

    Bump version and API version

--- MementoRefresh/MementoRefresh.lua
+++ MementoRefresh/MementoRefresh.lua
@@ -1,7 +1,7 @@
 MementoRefresh = MementoRefresh or {
   name = "MementoRefresh",
   author = "@Pretz333 (NA)",
-  version = "0.1.6",
+  version = "0.1.7",
   variableVersion = 2,
   defaults = {
     mementoId = nil,
